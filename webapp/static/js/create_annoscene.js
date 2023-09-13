f5b0e3da72a2373e942364ed5b7456465ada41be
brianlan <brianlanbo@gmail.com>
2023-09-13 19:24:30 +0800

    Added check for the duplicated scene_name when creating new scene

--- webapp/static/js/create_annoscene.js
+++ webapp/static/js/create_annoscene.js
@@ -76,24 +76,33 @@ let new_annoscene = new Vue({
                 return;
             }
 
-            // TODO: check if the same scene_name has been exist in the DB, if so, alert and return
-
-            headers = {'Content-Type': 'application/json'};
-            let new_scene = {
-                "name": self.scene_name,
-                "desc": self.scene_desc,
-                "classes": self.selected_class_ids
-            };
-            axios.post("/annoscene", new_scene, {headers: headers})  // call eve rest api
+            // check if the same scene_name has been exist in the DB, if so, alert and return
+            // here is how we do it:
+            // 1. query the RESTAPI /annoscene?where={"name": "self.scene_name"}
+            // 2. see if the returned data is empty, if not, alert and return
+            axios.get(`/annoscene?where={"name": "${self.scene_name}"}`)
                 .then(function (response) {
-                    alert("成功创建标注场景");
-                    
-                    // jump to all_annoscenes page
-                    window.location.href = "/all_annoscenes";
-                    
-                }).catch(function (error) {
-                    alert(JSON.stringify(error));
-            });
+                    if (response.data._items.length === 0) {
+                        headers = {'Content-Type': 'application/json'};
+                        let new_scene = {
+                            "name": self.scene_name,
+                            "desc": self.scene_desc,
+                            "classes": self.selected_class_ids
+                        };
+                        axios.post("/annoscene", new_scene, {headers: headers})  // call eve rest api
+                            .then(function (response) {
+                                alert("成功创建标注场景");
+                                
+                                // jump to all_annoscenes page
+                                window.location.href = "/all_annoscenes";
+
+                            }).catch(function (error) {
+                                alert(JSON.stringify(error));
+                        });
+                    } else {
+                        alert(`场景名称${self.scene_name}已经存在，请换一个名字重试.`);
+                    }
+                })
         },
         toggle_attr: function (annoclass_name, attr_name) {
             this.$set(this.show, annoclass_name + "-" + attr_name, !this.show[annoclass_name + "-" + attr_name])
